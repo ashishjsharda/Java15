d612993021271a8f87e882c8600e281e0d465fb9
Ashish S <ashishjsharda@gmail.com>
2021-05-30 20:58:43 -0400

    Create Multiline.java

---
+++ Multiline.java
@@ -0,0 +1,15 @@
+/**
+ * Example showing multiline text
+ * @author ashish
+ */
+public class Multiline {
+    public static void main(String[] args) {
+
+        String text= """
+                This is an example \
+                showing multiline text
+                """;
+        System.out.println(text);
+
+    }
+}
